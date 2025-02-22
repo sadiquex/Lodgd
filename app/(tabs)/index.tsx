b1f4d06ef67dea63e1c77e82d27df1e3d302cf58
sadiquex <abubakasaddik1@gmail.com>
2025-02-22 22:18:54 +0000

    setup listings data fetching

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,23 +3,23 @@ import React, { useEffect, useState } from "react";
 import { Link, Stack } from "expo-router";
 import ExploreHeader from "@/components/explore-header";
 import Listings from "@/components/listings";
-import axios from "axios";
+// import axios from "axios";
 
 export default function Homepage() {
   const [category, setCategory] = useState("Tiny homes");
   const [listings, setListings] = useState([]);
 
   // make the api call here
-  const getListings = () => {
-    const data = axios.get(
-      `https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/air-bnb-listings/records?limit=20`
-    );
-    console.log("🚀 ~ getListings ~ data:", data);
-  };
+  // const getListings = () => {
+  //   const data = axios.get(
+  //     `https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/air-bnb-listings/records?limit=20`
+  //   );
+  //   console.log("🚀 ~ getListings ~ data:", data);
+  // };
 
-  useEffect(() => {
-    // getListings();
-  }, []);
+  // useEffect(() => {
+  //   // getListings();
+  // }, []);
 
   const onDataChanged = (category: string) => {
     // console.log("🚀 ~ onDataChanged ~ category:", category);
